c8aaabd7cd144d53be8cda1875c0559d87872eff
Kai Henry <mug3tsuu@gmail.com>
2020-11-28 16:26:46 +0000

    diagonal movement

--- Player.cpp
+++ Player.cpp
@@ -20,7 +20,7 @@ void Player::Move(std::unordered_map<std::string, std::unique_ptr<RenderItem>> &
 	XMStoreFloat4x4(&mAllRitems["Player"]->Instances.at(0).World, transform);
   }
 
-  else if (GetAsyncKeyState(VK_RIGHT) & 0x8000)
+  if (GetAsyncKeyState(VK_RIGHT) & 0x8000)
   {
 	// TODO: WORKING MOVEMENT TEST LOCATED HERE
 	XMMATRIX transform = XMMatrixMultiply(XMMatrixIdentity(), XMMatrixTranslation(5.0f * dt, 0.0f, 0.0f));
@@ -30,7 +30,7 @@ void Player::Move(std::unordered_map<std::string, std::unique_ptr<RenderItem>> &
   }
 
 
-  else if (GetAsyncKeyState(VK_UP) & 0x8000)
+  if (GetAsyncKeyState(VK_UP) & 0x8000)
   {
 	// TODO: WORKING MOVEMENT TEST LOCATED HERE
 	XMMATRIX transform = XMMatrixMultiply(XMMatrixIdentity(), XMMatrixTranslation(0.0f, 0.0f, 5.0f * dt));
@@ -39,10 +39,49 @@ void Player::Move(std::unordered_map<std::string, std::unique_ptr<RenderItem>> &
 	XMStoreFloat4x4(&mAllRitems["Player"]->Instances.at(0).World, transform);
   }
 
-  else if (GetAsyncKeyState(VK_DOWN) & 0x8000)
+  if (GetAsyncKeyState(VK_DOWN) & 0x8000)
   {
 	// TODO: WORKING MOVEMENT TEST LOCATED HERE
 	XMMATRIX transform = XMMatrixMultiply(XMMatrixIdentity(), XMMatrixTranslation(0.0f, 0.0f, -5.0f * dt));
+	XMMATRIX current = XMLoadFloat4x4(&mAllRitems["Player"]->Instances.at(0).World);
+	transform = XMMatrixMultiply(current, transform);
+	XMStoreFloat4x4(&mAllRitems["Player"]->Instances.at(0).World, transform);
+  }
+
+
+  if ((GetAsyncKeyState(VK_DOWN) & 0x8000) && ((GetAsyncKeyState(VK_LEFT) & 0x8000)))
+  {
+	// TODO: WORKING MOVEMENT TEST LOCATED HERE
+	XMMATRIX transform = XMMatrixMultiply(XMMatrixIdentity(), XMMatrixTranslation (-1.0f * dt, 0.0f, -5.0f * dt));
+	XMMATRIX current = XMLoadFloat4x4(&mAllRitems["Player"]->Instances.at(0).World);
+	transform = XMMatrixMultiply(current, transform);
+	XMStoreFloat4x4(&mAllRitems["Player"]->Instances.at(0).World, transform);
+  }
+
+  if ((GetAsyncKeyState(VK_DOWN) & 0x8000) && ((GetAsyncKeyState(VK_RIGHT) & 0x8000)))
+  {
+	// TODO: WORKING MOVEMENT TEST LOCATED HERE
+	XMMATRIX transform = XMMatrixMultiply(XMMatrixIdentity(), XMMatrixTranslation(1.0f * dt, 0.0f, -5.0f * dt));
+	XMMATRIX current = XMLoadFloat4x4(&mAllRitems["Player"]->Instances.at(0).World);
+	transform = XMMatrixMultiply(current, transform);
+	XMStoreFloat4x4(&mAllRitems["Player"]->Instances.at(0).World, transform);
+  }
+
+
+  if ((GetAsyncKeyState(VK_UP) & 0x8000) && ((GetAsyncKeyState(VK_RIGHT) & 0x8000)))
+  {
+	// TODO: WORKING MOVEMENT TEST LOCATED HERE
+	XMMATRIX transform = XMMatrixMultiply(XMMatrixIdentity(), XMMatrixTranslation(1.0f * dt, 0.0f, 5.0f * dt));
+	XMMATRIX current = XMLoadFloat4x4(&mAllRitems["Player"]->Instances.at(0).World);
+	transform = XMMatrixMultiply(current, transform);
+	XMStoreFloat4x4(&mAllRitems["Player"]->Instances.at(0).World, transform);
+  }
+
+
+  if ((GetAsyncKeyState(VK_UP) & 0x8000) && ((GetAsyncKeyState(VK_LEFT) & 0x8000)))
+  {
+	// TODO: WORKING MOVEMENT TEST LOCATED HERE
+	XMMATRIX transform = XMMatrixMultiply(XMMatrixIdentity(), XMMatrixTranslation(-1.0f * dt, 0.0f, 5.0f * dt));
 	XMMATRIX current = XMLoadFloat4x4(&mAllRitems["Player"]->Instances.at(0).World);
 	transform = XMMatrixMultiply(current, transform);
 	XMStoreFloat4x4(&mAllRitems["Player"]->Instances.at(0).World, transform);
