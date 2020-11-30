d180f4187d203e69b680036b3dadbfe8f9184ef4
LUC HARRIS <b8017041@hallam.shu.ac.uk>
2020-11-30 12:37:12 +0000

    Seperated WinMain to its own cpp

--- GameApp.cpp
+++ GameApp.cpp
@@ -15,31 +15,7 @@ using namespace DirectX::PackedVector;
 #pragma comment(lib, "d3dcompiler.lib")
 #pragma comment(lib, "D3D12.lib")
 
-const int gNumFrameResources = 3;
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
-	PSTR cmdLine, int showCmd)
-{
-	// Enable run-time memory check for debug builds.
-#if defined(DEBUG) | defined(_DEBUG)
-	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-#endif
-
-
-	try
-	{
-		GameApp theApp(hInstance);
-		if (!GameApp::Get().Initialize())
-			return 0;
-
-		return GameApp::Get().Run();
-	}
-	catch (DxException& e)
-	{
-		MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
-		return 0;
-	}
-}
+const int gNumFrameResources = 3; //todo move to GC namespace in its own header
 
 GameApp::GameApp(HINSTANCE hInstance)
 	: D3DApp(hInstance)
---
+++ WinMain.cpp
@@ -0,0 +1,28 @@
+
+//#include "WindowUtils.h"
+#include "GameApp.h"
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
+	PSTR cmdLine, int showCmd)
+{
+	// Enable run-time memory check for debug builds.
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+
+	try
+	{
+		GameApp theApp(hInstance);
+		if (!GameApp::Get().Initialize())
+			return 0;
+
+		return GameApp::Get().Run();
+	}
+	catch (DxException& e)
+	{
+		MessageBox(nullptr, e.ToString().c_str(), L"HR Failed", MB_OK);
+		return 0;
+	}
+}
